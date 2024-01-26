d9e609b3afa4b400f7fdb031d55f4914bf38bd68
mariehposa <mariamadedeji.work@gmail.com>
2024-01-26 22:34:18 +0000

    set up apollo client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useQuery, gql } from '@apollo/client';
 import logo from './logo.svg';
 import './App.css';
 
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  console.log(data, 'graphql data');
+  
   return (
     <div className="App">
       <header className="App-header">
