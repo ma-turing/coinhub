43827781cf3785ae8ecdaca8658aa848d946ea0c
mariehposa <mariamadedeji.work@gmail.com>
2024-01-30 17:48:37 +0000

    create color variables

---
+++ src/utils/color.js
@@ -0,0 +1,9 @@
+export const colorTypes = {
+    primary: "#23A6F0",
+    text: "#252B42",
+    second_text: "#737373",
+    light_text: "#FFFFFF",
+    success: "#2DC071",
+    muted: "#BDBDBD",
+    rating: "#F3CD03"
+}
