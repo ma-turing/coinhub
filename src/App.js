cdee47bde5b44d1939df51ddc038d5be58689eee
mariehposa <mariamadedeji.work@gmail.com>
2024-01-31 16:05:44 +0000

    import Header

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import { useQuery, gql } from '@apollo/client';
-import logo from './logo.svg';
-import './App.css';
-
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
+import Header from "./views/Header/Header";
 
 function App() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
-  console.log(data, 'graphql data');
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+				<Header />
+		</div>
+	);
 }
 
 export default App;
