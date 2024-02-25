41b4874a6d757cc027763b276a6e0e95c2ae2585
mariehposa <mariamadedeji.work@gmail.com>
2024-02-25 14:34:23 +0000

    restructure app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Footer from "./views/Footer/Footer";
 import Header from "./views/Header/Header";
 import Home from "./views/Home/Home";
 import Error from "./views/Error/Error";
+import "./App.css";
 
 function App() {
 	return (
 		<Router>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-          <Route path="*" element={<Error />} />
-				</Routes>
-        <Footer />
-			</Router>
+			<Header />
+			<Routes>
+				<Route path="/*" element={<Home />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
+			<Footer />
+		</Router>
 	);
 }
 
