acea19ec734978d83914e16451e7988fd120920b
mariehposa <mariamadedeji.work@gmail.com>
2024-03-04 22:05:48 +0000

    add dummy data

---
+++ src/utils/pricingData.js
@@ -0,0 +1,28 @@
+export const pricingData = [
+	{
+		heading: "free",
+		subheading: "Organize across all apps by hand",
+		price: 0,
+		features: [
+			{ text: "Unlimited product updates", checked: true },
+			{ text: "Unlimited product updates", checked: true },
+			{ text: "Unlimited product updates", checked: true },
+            { text: "1GB Cloud Storage", checked: false },
+            { text: "Email and community support", checked: false }
+		],
+		btn_text: "Try for free",
+	},
+	{
+		heading: "standard",
+		subheading: "Organize across all apps by hand",
+		price: 19,
+		features: [
+			{ text: "Unlimited product updates", checked: true },
+			{ text: "Unlimited product updates", checked: true },
+			{ text: "Unlimited product updates", checked: true },
+            { text: "1GB Cloud Storage", checked: true },
+            { text: "Email and community support", checked: true }
+		],
+		btn_text: "Buy Now"
+	}
+];
