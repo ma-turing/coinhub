ef55c47513dbb786ac02526893f794e2e6b9eef7
mariehposa <mariamadedeji.work@gmail.com>
2024-03-04 22:05:19 +0000

    update btn props

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const StyledButton = styled.button`
     background-color: ${(props) => (props.primary ? "#23A6F0" : "#FFF")};
     color: ${(props) => (props.primary ? "#FFF" : "#23A6F0")};
-    padding: ${(props) => props.large ? "15px 50px" : "15px 30px"};
+    padding: ${(props) => props.large ? "15px 107px" : props.medium ? "15px 50px" : "15px 30px"};
     border: ${(props) => (props.borderless ? "none" : "1px solid #23A6F0")};
     border-radius: ${(props) => (props.rounded ? "50px" : "10px")};
     font-size: 15px;
