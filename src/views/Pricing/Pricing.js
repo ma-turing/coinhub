3162ea72028aa24a0a38b5d1553cc403b4934d3c
mariehposa <mariamadedeji.work@gmail.com>
2024-03-04 22:06:07 +0000

    implement pricing section

--- src/views/Pricing/Pricing.js
+++ src/views/Pricing/Pricing.js
@@ -1,9 +1,20 @@
+import SectionHeaders from "../../components/SectionHeaders/SectionHeaders";
+import PricingCard from "../../components/PricingCard/PricingCard";
+import { pricingData } from "../../utils/pricingData";
 import "./Pricing.css";
 
 const Pricing = () => {
 	return (
-		<div id="pricing">
-			<p>Pricing page</p>
+		<div className="pricing-container" id="pricing">
+			<SectionHeaders
+				heading="Pricing"
+				subheading={`Problems trying to resolve the conflict between \\\\n the two major realms of Classical physics: Newtonian mechanics `}
+			/>
+			<div className="prices">
+				{pricingData.map((data, index) => (
+					<PricingCard key={index} data={data} />
+				))}
+			</div>
 		</div>
 	);
 };
