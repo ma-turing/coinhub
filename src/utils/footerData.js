264cde038c5a9302503a2fe441b31bcbee8c505c
mariehposa <mariamadedeji.work@gmail.com>
2024-03-05 23:58:34 +0000

    add footer data

---
+++ src/utils/footerData.js
@@ -0,0 +1,29 @@
+export const footerData = [
+	{
+		header: "Company Info",
+		nav_items: [
+			{ text: "About us", link: "about-us" },
+			{ text: "Carrier", link: "" },
+			{ text: "We are hiring", link: "" },
+			{ text: "Blog", link: "" },
+		],
+	},
+	{
+		header: "Features",
+		nav_items: [
+			{ text: "Business Marketing", link: "" },
+			{ text: "User Analytic", link: "" },
+			{ text: "Product", link: "product" },
+			{ text: "Unlimited Support", link: "" },
+		],
+	},
+	{
+		header: "Resources",
+		nav_items: [
+			{ text: "Review", link: "review" },
+			{ text: "Watch a Demo", link: "" },
+			{ text: "Customers", link: "" },
+			{ text: "API", link: "" },
+		],
+	},
+];
