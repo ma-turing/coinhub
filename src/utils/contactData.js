59a982946429dbef17d4a9bb5bebf50e949b6f4c
mariehposa <mariamadedeji.work@gmail.com>
2024-03-05 23:58:43 +0000

    add contact data

---
+++ src/utils/contactData.js
@@ -0,0 +1,14 @@
+export const contactData = [
+	{
+		header: "coinhub card",
+		issues: ["Card activation problem", "Transaction declines", "Fraudulent activity", "Card delivery delays"],
+	},
+	{
+		header: "coinhub wallet",
+		issues: ["Loss of funds", "Unsupported token", "Fee transparency", "Transaction failures"],
+	},
+	{
+		header: "coinhub one",
+		issues: ["Fees and costs", "Market liquidity", "Platform reliabilty"],
+	},
+]
