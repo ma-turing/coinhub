114b53d15f52acd9538bfb58e7964bde5c9d4604
mariehposa <mariamadedeji.work@gmail.com>
2024-03-26 16:52:40 +0000

    get asset history per minute

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -6,14 +6,26 @@ export const getCurrentLimit = (value) => ({
     payload: value
 })
 
-export const getCryptocurrencies = (currentLimit) => {
+export const getAssets = (currentLimit) => {
     return async (dispatch) => {
-        dispatch({type: types.GET_CRYPTO_START})
+        dispatch({type: types.GET_ASSETS_START})
         try {
             const response = await axios.get(`${process.env.REACT_APP_API_URL}/assets`, {params: {limit: currentLimit}})
-            dispatch({type: types.GET_CRYPTO_SUCCESS, payload: response.data.data})
+            dispatch({type: types.GET_ASSETS_SUCCESS, payload: response.data.data})
         } catch (error) {
-            dispatch({type: types.GET_CRYPTO_FAIL, payload: error.message})
+            dispatch({type: types.GET_ASSETS_FAIL, payload: error.message})
+        }
+    }
+}
+
+export const getAssetHistory = (assetId) => {
+    return async (dispatch) => {
+        dispatch({type: types.GET_ASSET_HISTORY_START})
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/assets/${assetId}/history`, {params: {interval: "m1"}})
+            dispatch({type: types.GET_ASSET_HISTORY_SUCCESS, payload: response.data.data})
+        } catch (error) {
+            dispatch({type: types.GET_ASSET_HISTORY_FAIL, payload: error.message})
         }
     }
 }
