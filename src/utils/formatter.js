0537ca0ed66c6676e0c115449eb78b949aaf4e23
mariehposa <mariamadedeji.work@gmail.com>
2024-03-26 15:47:35 +0000

    create data formatters

---
+++ src/utils/formatter.js
@@ -0,0 +1,19 @@
+export const formatNumber = (num) => {
+    return num.toLocaleString(undefined, { maximumFractionDigits: 2 });
+};
+
+export const formatCurrency = (num) => {
+    if (num >= 1e12) {
+        return (num / 1e12).toFixed(2) + "t";
+    } else if (num >= 1e9) {
+        return (num / 1e9).toFixed(2) + "b";
+    } else if (num >= 1e6) {
+        return (num / 1e6).toFixed(2) + "m";
+    } else {
+        return formatNumber(num);
+    }
+};
+
+export const formatPercentage = (num) => {
+    return formatNumber(num) + "%";
+};
