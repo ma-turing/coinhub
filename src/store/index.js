7dcf7a45153b59d4d01ea9d22a3fb0c7e735dbcf
mariehposa <mariamadedeji.work@gmail.com>
2024-03-26 16:53:02 +0000

    fix import

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import rootReducer from "../redux/reducer";
+import rootReducer from "../redux/reducer/reducer";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({ reducer: rootReducer });
